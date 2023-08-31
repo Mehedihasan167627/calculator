f8362396b94bada4fec183335b833d5ea9b23f1a
Mehedi Hasan <mehedinahid27@gmail.com>
2023-08-31 21:35:29 +0600

    modified

--- core/settings.py
+++ core/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-vf9@toqow557d7097fd@121c=80k5iw!$9ggc5(+u(g&1)r@mm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -104,9 +104,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
+import os 
 STATIC_URL = 'static/'
-
+# STATIC_ROOT=BASE_DIR / "static"
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static')
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
