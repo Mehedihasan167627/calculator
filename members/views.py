a88f5ce819e9bcb27a34c9f5ed6d7f63de77c6a2
Mehedi Hasan <mehedinahid27@gmail.com>
2023-09-02 23:02:50 +0600

    added scheduler

--- members/views.py
+++ members/views.py
@@ -4,51 +4,55 @@ from django.http import JsonResponse
 from .models import MealRequestClose,Meal
 import time
 from datetime import datetime ,timedelta
-import schedule
-import threading
+# import schedule
+# import threading
 
 
-class MyThread(threading.Thread):
-    def __init__(self, delay=1):
-        super().__init__()
-        self.delay = delay
+# class MyThread(threading.Thread):
+#     def __init__(self, delay=1):
+#         super().__init__()
+#         self.delay = delay
 
-    def run(self):
-        while True: 
-            print("called")
-            time.sleep(self.delay)
+#     def run(self):
+#         while True: 
+#             job()
+#             print("called")
+#             time.sleep(self.delay)
 
-def job():
-    continue_meal=Meal.objects.filter(is_continue=True)
-    next_day=datetime.now().date()+timedelta(days=1)
-    desired_time = "21:30"
-    close=MealRequestClose.objects.last()
-    if close:
-        desired_time=close.time 
-    current_time = time.strftime("%H:%M")
-    if current_time == desired_time:
-        check=Meal.objects.filter(date=next_day)
-        if not check:
-            for meal in continue_meal:
-                Meal.objects.create(
-                    member=meal.member,
-                    breakfast=meal.breakfast,
-                    launch=meal.launch,
-                    dinner=meal.dinner,
-                    is_continue=meal.is_continue,
-                    date=next_day
-                )
+# def job():
+#     next_day=datetime.now().date()+timedelta(days=1)
+#     continue_meal=Meal.objects.filter(is_continue=True,date=datetime.now().date())
+
+#     desired_time = "23:01"
+
+#     # close=MealRequestClose.objects.last()
+#     # if close:
+#     #     desired_time=close.time 
+#     current_time = time.strftime("%H:%M")
+#     print(current_time)
+#     if current_time == desired_time:
+#         for meal in continue_meal:
+#                 Meal.objects.create(
+#                     member=meal.member,
+#                     breakfast=meal.breakfast,
+#                     launch=meal.launch,
+#                     dinner=meal.dinner,
+#                     is_continue=meal.is_continue,
+#                     date=next_day
+#                 )
+#         print("created")
+    
     
 
-schedule.every(1).seconds.do(job)
+# schedule.every(1).seconds.do(job)
 
 class MealRequestView(View):
     def get(self,request):
         return render(request,"members/index.html")
     
     def post(self,request):
-        reader=MyThread(30)
-        reader.start()
+        # reader=MyThread(1)
+        # reader.start()
         return JsonResponse({"data":"ok"})
 
 
